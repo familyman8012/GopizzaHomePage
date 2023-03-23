aa40ab18cc03b271e85dad575b373cdc2ce52328
yuneunsuk <familyman80@naver.com>
2023-03-23 12:39:15 +0900

    캠페인 내림

--- components/layouts/Top.tsx
+++ components/layouts/Top.tsx
@@ -11,9 +11,9 @@ function Top({ className }: { className?: string }) {
   };
   return (
     <TopWrap className={className}>
-      <a target="_blank" href="/event/campaign/infiltration" rel="noreferrer" className="floating_bnr">
+      {/* <a target="_blank" href="/event/campaign/infiltration" rel="noreferrer" className="floating_bnr">
         <span className="hiddenZoneV">2023 전국! 고피자 침투! 이벤트 참여하기</span>
-      </a>
+      </a> */}
       <button onClick={scrollToTop}>TOP</button>
     </TopWrap>
   );
