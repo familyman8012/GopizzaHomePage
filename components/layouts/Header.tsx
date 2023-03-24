ac215a7220e396cbb1d47d092ee9e199490c74ab
yuneunsuk <familyman80@naver.com>
2023-03-24 14:50:03 +0900

    띠배너 적용

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,8 +1,27 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
 import { HeadMenuUrl, HeadAsideUrl } from "./constant";
 import { HeaderWrap } from "./style";
+
+export const TopBanner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 31px;
+  padding: 0 13.5px;
+  background-color: #171c8f;
+
+  span {
+    color: #ffce00;
+    font-size: 14px;
+    line-height: 13px;
+
+    padding-left: 22.5px;
+    background: url("/images/common/ico_mobile_not.svg") no-repeat left top / 14.5px 13px;
+  }
+`;
 
 function Header() {
   const router = useRouter();
@@ -12,6 +31,9 @@ function Header() {
   }, []);
   return (
     <>
+      <TopBanner>
+        <span className="txt">모바일 최적화 준비 중에 있습니다.</span>
+      </TopBanner>
       <HeaderWrap className={hiddenPopMobile ? `off` : ""}>
         <div className="inner">
           <Link href="/main">
@@ -42,24 +64,6 @@ function Header() {
               </Link>
             ))}
           </aside>
-        </div>
-        <div className="pop_mobile_not">
-          <div className="inner_content">
-            <div className="box_txt">
-              <h2>모바일 최적화 준비중</h2>
-              <p>
-                현재는 모바일 최적화 준비중으로
-                <br />
-                모바일 환경을 지원하고 있지 않습니다.
-              </p>
-            </div>
-            <button className="btn_back" onClick={() => router.back()}>
-              뒤로가기
-            </button>
-            <button className="show_page" onClick={() => handlerShowPopMobile()}>
-              웹 페이지로 살펴보기 &gt;
-            </button>
-          </div>
         </div>
       </HeaderWrap>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { ReactElement, useCallback, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { TopBanner } from "ComponentsFarm/layouts/Header";
 
 function Index() {
   const router = useRouter();
@@ -11,51 +12,38 @@ function Index() {
     document.body.classList.add("webShow");
   }, []);
   return (
-    <IndexWrap className={hiddenPopMobile ? `off` : ""}>
-      <div className="box_goto">
-        <div className="inner">
-          <h1>BRAND</h1>
-          <p>
-            고피자의 메뉴와
-            <br />
-            매장을 찾고 계신가요?
-          </p>
-          <Link href="/main">
-            <span className="txt">홈페이지</span>
-          </Link>
-        </div>
-      </div>
-      <div className="box_goto">
-        <div className="inner">
-          <h1>FRANCHISE</h1>
-          <p>
-            고피자 창업을
-            <br /> 고민하고 계신가요?
-          </p>
-          <Link href="/start">
-            <span className="txt">프랜차이즈</span>
-          </Link>
-        </div>
-      </div>
-      <div className="pop_mobile_not">
-        <div className="inner_content">
-          <div className="box_txt">
-            <h2>모바일 최적화 준비중</h2>
+    <>
+      <TopBanner>
+        <span className="txt">모바일 최적화 준비 중에 있습니다.</span>
+      </TopBanner>
+      <IndexWrap className={hiddenPopMobile ? `off` : ""}>
+        <div className="box_goto">
+          <div className="inner">
+            <h1>BRAND</h1>
             <p>
-              현재는 모바일 최적화 준비중으로
+              고피자의 메뉴와
               <br />
-              모바일 환경을 지원하고 있지 않습니다.
+              매장을 찾고 계신가요?
             </p>
+            <Link href="/main">
+              <span className="txt">홈페이지</span>
+            </Link>
           </div>
-          <button className="btn_back" onClick={() => router.back()}>
-            뒤로가기
-          </button>
-          <button className="show_page" onClick={() => handlerShowPopMobile()}>
-            웹 페이지로 살펴보기 &gt;
-          </button>
         </div>
-      </div>
-    </IndexWrap>
+        <div className="box_goto">
+          <div className="inner">
+            <h1>FRANCHISE</h1>
+            <p>
+              고피자 창업을
+              <br /> 고민하고 계신가요?
+            </p>
+            <Link href="/start">
+              <span className="txt">프랜차이즈</span>
+            </Link>
+          </div>
+        </div>
+      </IndexWrap>
+    </>
   );
 }
 
