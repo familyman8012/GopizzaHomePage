7964c6c8f7ada550a21b0441079ebcf8cf981645
yuneunsuk <familyman80@naver.com>
2023-04-05 14:39:08 +0900

    가맹문의 영상을 창업경쟁력 영상으로

--- components/pageComp/start/technology/style.ts
+++ components/pageComp/start/technology/style.ts
@@ -70,35 +70,6 @@ export const GopizzaTechWrap = styled.section`
   }
 `;
 
-export const VideoWrap = styled.div`
-  position: relative;
-  margin: 10.6rem auto 0;
-
-  .box_img {
-    position: relative;
-    width: 128rem;
-    height: 72rem;
-
-    iframe {
-      border-radius: 30px;
-    }
-  }
-
-  .react-player__play-icon {
-    display: none;
-  }
-
-  button {
-    position: absolute;
-    width: 9.3rem;
-    height: 9.3rem;
-    top: 50%;
-    left: 50%;
-    background: url("https://dev-gopizza-homepage.s3.ap-northeast-2.amazonaws.com/ui/images/start/tech/ico_play.svg") no-repeat left top / 100%;
-    transform: translate(-50%, -50%);
-  }
-`;
-
 export const NeedWrap = styled.section`
   position: relative;
   width: 128rem;
