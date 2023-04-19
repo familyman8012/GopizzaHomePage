7e3c0f03e72d1a2d96bc3bbfa9e76c30faf5789d
yuneunsuk <familyman80@naver.com>
2023-04-19 10:32:49 +0900

    구글 리마케팅

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,17 @@ export default function Document() {
       <Head>
         {/* <link rel="stylesheet" href="https://use.typekit.net/kfw2qcs.css" /> */}
         <script defer src="https://www.youtube.com/player_api"></script>
-        <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAOMAP_APIKEY}&libraries=services,clusterer&autoload=false`}
-          strategy="beforeInteractive"
-        />
+        <Script src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAOMAP_APIKEY}&libraries=services,clusterer&autoload=false`} strategy="beforeInteractive" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11121440258" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'AW-11121440258');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
