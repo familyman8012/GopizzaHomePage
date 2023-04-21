9b83eef299bd725410171a98ed5712cbc9d84fed
yuneunsuk <familyman80@naver.com>
2023-04-21 13:28:04 +0900

    개설비용안내 sale 삭제

--- components/pageComp/start/guide/Cost.tsx
+++ components/pageComp/start/guide/Cost.tsx
@@ -74,6 +74,90 @@ function Cost() {
           <tr>
             <td>가맹비</td>
             <td>
+              <span>5,000,000</span>
+            </td>
+            <td>
+              <span>5,000,000</span>
+            </td>
+          </tr>
+          <tr>
+            <td>교육비</td>
+            <td>
+              <span>3,000,000</span>
+            </td>
+            <td>
+              <span>3,000,000</span>
+            </td>
+          </tr>
+          <tr>
+            <td>계약이행보증금</td>
+            <td>
+              <span>5,000,000</span>
+            </td>
+            <td>
+              <span>5,000,000</span>
+            </td>
+          </tr>
+          <tr>
+            <td>GOVEN 자동화덕</td>
+            <td>8,900,000</td>
+            <td>8,900,000</td>
+          </tr>
+          <tr>
+            <td>장비 및 기물 비용</td>
+            <td>18,500,000</td>
+            <td>14,500,000</td>
+          </tr>
+          <tr>
+            <td>인테리어</td>
+            <td>55,000,000</td>
+            <td>27,000,000</td>
+          </tr>
+          <tr>
+            <td>별도 공사</td>
+            <td>매장별 상이</td>
+            <td>매장별 상이</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>합계(VAT별도)</td>
+            <td>
+              <span className="box_price">
+                {/* <span className="price">95,400,000</span> */}
+                <span className="sale_price">85,400,000</span>
+              </span>
+            </td>
+            <td>
+              <span className="box_price">
+                {/* <span className="price">63,400,000</span> */}
+                <span className="sale_price">53,400,000</span>
+              </span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+      {/* <table>
+        <caption>개설비용안내표</caption>
+        <thead>
+          <tr>
+            <th>
+              <span className="hiddenZoneV">비용 항목</span>
+            </th>
+            <th>
+              <span className="txt1">일반매장</span>
+              <span className="txt2">15평 기준(vat별도)</span>
+            </th>
+            <th>
+              <span className="txt1">배달전용매장</span>
+              <span className="txt2">9평 기준 (vat별도)</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>가맹비</td>
+            <td>
               <span className="sale sale2">5,000,000</span>
             </td>
             <td>
@@ -136,7 +220,7 @@ function Cost() {
             </td>
           </tr>
         </tfoot>
-      </table>
+      </table> */}
     </CostWrap>
   );
 }
