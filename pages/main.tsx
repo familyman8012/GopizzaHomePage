c7c2557160d3e72ef0fa95c0dd568472ed808fb8
futureplanning <futureplanning@gopizza.kr>
2023-05-02 16:11:07 +0900

    메인페이지 창업비 네고

--- pages/main.tsx
+++ pages/main.tsx
@@ -8,6 +8,7 @@ import LinkBanner from "ComponentsFarm/pageComp/main/LinkBanner";
 import MainVisual from "ComponentsFarm/pageComp/main/MainVisual";
 import Order from "ComponentsFarm/pageComp/main/Order";
 import Youtube from "ComponentsFarm/pageComp/main/Youtube";
+import TodayPopup from "ComponentsFarm/popup/TodayPopup";
 
 interface IMain {
   mainVisual: IMainVisual;
@@ -17,15 +18,18 @@ interface IMain {
 
 function Main({ mainVisual, youtube, feed }: IMain) {
   return (
-    <main className="index">
-      <MainVisual data={mainVisual} />
-      <LinkBanner />
-      <BestMenu />
-      <Competitiveness />
-      <Order />
-      <Instagram feed={feed} />
-      <Youtube data={youtube} />
-    </main>
+    <>
+      <main className="index">
+        <MainVisual data={mainVisual} />
+        <LinkBanner />
+        <BestMenu />
+        <Competitiveness />
+        <Order />
+        <Instagram feed={feed} />
+        <Youtube data={youtube} />
+      </main>
+      <TodayPopup href="https://youtu.be/Dwa4zg-tC0s" src="/images/start/info/pop_guide.png" alt="국내최초 창업비 네고!!! 1500만원 네고! 선착순 프로모션 혜택 보러가기" />
+    </>
   );
 }
 
