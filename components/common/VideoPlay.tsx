bab661c4119d47bb8c19fa6fe3d891cc6f5db5dc
futureplanning <futureplanning@gopizza.kr>
2023-06-23 17:41:03 +0900

    이벤트 비디오 페이지 모바일 성능 최적화

--- components/common/VideoPlay.tsx
+++ components/common/VideoPlay.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 import { VideoPlayWrap } from "./style";
 import ReactPlayer from "react-player/lazy";
+import { useMediaQuery } from "react-responsive";
+
 interface IVideoPlay {
   imgUrl: string;
   txt1: string;
@@ -21,59 +23,69 @@ function VideoPlay({ videoUrl, imgUrl, txt1 }: IVideoPlay) {
     setWinReady(true);
   }, []);
 
+  const [isClient, setIsClient] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <>
-      <VideoPlayWrap className="mobile_view">
-        <a target="_blank" rel="noreferrer" href={videoUrl}>
-          <div className="video_bg">
+      {isClient && isMobile ? (
+        <VideoPlayWrap className="mobile_view">
+          <a target="_blank" rel="noreferrer" href={videoUrl}>
+            <div className="video_bg">
+              <div className="thumb">
+                <img src={imgUrl} alt={txt1} />
+              </div>
+            </div>
+          </a>
+        </VideoPlayWrap>
+      ) : (
+        <VideoPlayWrap className="pc_view" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <div style={{ position: "absolute", top: 0, left: 0, zIndex: 2, opacity: play ? 1 : 0 }}>
+            {winReady && (
+              <ReactPlayer
+                width="100%"
+                height="100%"
+                playing={play}
+                muted={true}
+                controls={false}
+                opts={{
+                  playerVars: {
+                    rel: 0, //관련 동영상 표시하지 않음. 유튜브 api 변경이후, 강제 관련 동영상이 나타남.
+                    modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
+                    loop: 0,
+                  },
+                }}
+                url={`https://www.youtube.com/watch?v=${videoUrl.split("/")[3]}`}
+              />
+            )}
+          </div>
+
+          {play && (
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={videoUrl}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                zIndex: 2,
+                width: "100%",
+                height: "100%",
+              }}
+            ></a>
+          )}
+          <div className="video_bg" style={{ position: "relative", zIndex: 1, opacity: play ? 0 : 1 }}>
             <div className="thumb">
               <img src={imgUrl} alt={txt1} />
             </div>
           </div>
-        </a>
-      </VideoPlayWrap>
-      <VideoPlayWrap className="pc_view" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        <div style={{ position: "absolute", top: 0, left: 0, zIndex: 2, opacity: play ? 1 : 0 }}>
-          {winReady && (
-            <ReactPlayer
-              width="100%"
-              height="100%"
-              playing={play}
-              muted={true}
-              controls={false}
-              opts={{
-                playerVars: {
-                  rel: 0, //관련 동영상 표시하지 않음. 유튜브 api 변경이후, 강제 관련 동영상이 나타남.
-                  modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
-                  loop: 0,
-                },
-              }}
-              url={`https://www.youtube.com/watch?v=${videoUrl.split("/")[3]}`}
-            />
-          )}
-        </div>
-
-        {play && (
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={videoUrl}
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              zIndex: 2,
-              width: "100%",
-              height: "100%",
-            }}
-          ></a>
-        )}
-        <div className="video_bg" style={{ position: "relative", zIndex: 1, opacity: play ? 0 : 1 }}>
-          <div className="thumb">
-            <img src={imgUrl} alt={txt1} />
-          </div>
-        </div>
-      </VideoPlayWrap>
+        </VideoPlayWrap>
+      )}
     </>
   );
 }
--- pages/event/video.tsx
+++ pages/event/video.tsx
@@ -29,48 +29,22 @@ function Promotion(eventVideo: IEventVideo) {
 
   return (
     <EventLayout>
-      {isClient &&
-        (!isMobile ? (
-          <div className="mainvideo">
-            <div className="loader loader-7">
-              <div className="line line1"></div>
-              <div className="line line2"></div>
-              <div className="line line3"></div>
-            </div>
-            <iframe
-              src={`https://www.youtube.com/embed/${eventVideo?.list[0].youtube_url.split("/")[3]}?controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&playlist=${
-                eventVideo?.list[0].youtube_url.split("/")[3]
-              }`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="YouTube Video"
-            />
-          </div>
-        ) : (
-          <div className="video_wrap" style={{ marginTop: "-10.6rem" }}>
-            <VideoWrap>
-              <div className="box_img" onClick={handleToggle}>
-                {winReady && (
-                  <ReactPlayer
-                    light={<img src="/images/event/video/mobile/videox2.webp" alt="Thumbnail" />}
-                    url="https://www.youtube.com/watch?v=A-awWOnYf6I"
-                    width="100%"
-                    height="100%"
-                    playing={true}
-                    opts={{
-                      playerVars: {
-                        rel: 0,
-                        modestbranding: 1,
-                        loop: 0,
-                      },
-                    }}
-                  />
-                )}
-              </div>
-            </VideoWrap>
-          </div>
-        ))}
+      <div className="mainvideo">
+        <div className="loader loader-7">
+          <div className="line line1"></div>
+          <div className="line line2"></div>
+          <div className="line line3"></div>
+        </div>
+        <iframe
+          src={`https://www.youtube.com/embed/${eventVideo?.list[0].youtube_url.split("/")[3]}?controls=0&amp;autoplay=1&amp;mute=1&amp;loop=1&playlist=${
+            eventVideo?.list[0].youtube_url.split("/")[3]
+          }`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="YouTube Video"
+        />
+      </div>
 
       <ul className="list_event">
         {/* eventVideo.list */}
